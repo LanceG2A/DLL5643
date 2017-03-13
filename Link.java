6d54d60137400af423bc4ec58b271560bf7f5cd7
LGA2 <lackerson@albany.edu>
2017-03-12 22:01:07 -0400

    Comments

--- Link.java
+++ Link.java
@@ -5,19 +5,19 @@ public class Link { //Class to for Links (Nodes)
 	public Link next;	//and previous/next links(nodes)
 	public Link previous;
 	
-	public Link(String color)
+	public Link(String color) //Constructor initialializes link
 	{
 		previous = null;
 		this.colorName = color;
 		next = null;
 	}
-	public Link(Link previous, Link next, String colorName) {
+	public Link(Link previous, Link next, String colorName) { //Sets previous and next pointers
 		this.previous = previous;
 		this.next = next;
 		this.colorName = colorName;
 	}
 	
-	public String ReturnColor() {
+	public String ReturnColor() {  //Return link information
 		return this.colorName;
 	}
 	
