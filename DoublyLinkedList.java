52eaffd1fbe91c8b1a8d328ed1464c200ea7360a
LGA2 <lackerson@albany.edu>
2017-03-22 11:42:59 -0400

    Remove a node and find a node

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -4,13 +4,12 @@ public class DoublyLinkedList {
 	private Link head;
 	private int size;
 	
-	public DoublyLinkedList()
-	{
+	public DoublyLinkedList(){
 		head = null;
 		size = 0;
 	}
 	
-	public void addStart(String color)//to add a link in the list at the beggining
+	public void addStart(String color)//to add a link in the list at the beginning
 	{
 		if(head == null)		//checks if the list is empty
 		{						//if it is empty it sets the first Link to "color"
@@ -24,32 +23,116 @@ public class DoublyLinkedList {
 		}
 		size++;
 	}
-	public int size();
+	
+	public void addAlphabetical(String color){ //works now
+		if(head == null){
+			head = new Link(null, color, null);
+		}
+		else{
+		Link temp = head;
+			if(temp.next == null){
+				if(color.compareTo(temp.colorName) > 0){
+					Link newLink = new Link(head, color, null);
+					head.next = newLink;
+		}
+				else {
+					Link newLink = new Link(null, color, head);
+					head.previous = newLink;
+					head = newLink;
+		}
+		}
+			else{
+				int i = 0;
+				while(i == 0){
+					if(color.compareTo(temp.colorName) > 0){					
+						if(temp.next != null){
+							temp = temp.next;
+						}
+						else{
+							Link newLink = new Link(temp, color, temp.next);
+							Link temp2 = temp.next;
+							temp2.previous = newLink;
+							temp.next = newLink;
+							i++;
+						}
+			}
+				else{
+						Link newLink = new Link(temp.previous, color, temp);
+						if(temp.previous == null){
+							head = newLink;
+							temp.previous = newLink;
+							head.next = temp;
+						}
+						else{
+							temp.previous.next = newLink;
+							temp.previous = newLink;
+						}
+						i++;
+				}					
+						
+			}
+		}
+		}
+				
+			}
+			
+		
+		
+		
+	
+	
+	
+	public int size()
 	{
 		return size;
 	}
-	public void printList();{
+	public void printListForward(){
 		Link current = head;
 		while(current != null){
-			System.out.println(current);
+			System.out.println(current.colorName);
 			current = current.next;
 		}
+	}
+	public void printListBackward(){
+		Link current = head;
+		while(current.next != null){
+			current = current.next;
+		}
+		System.out.println(current.colorName);
+		while(current.previous != null){
+			System.out.println(current.previous.colorName);
+			current = current.previous;
+		}
+	}
 	
 	public boolean isEmpty(){
 		return head == null;	//returns true if head is null
 	}
 	
-	public void main(String[] args) //Main Method
-	{
-		DoublyLinkedList test = new DoublyLinkedList();
-		test.addStart("Blue");
-		test.addStart("Green");
-		test.printList();
+
+	
+	public void deleteLink(String color){ //deletes a node base on the color name it contains
+		Link temp = head;
+		while(temp.colorName != color){
+			temp = temp.next;
+		}
+		Link next = temp.next;
+		Link previous = temp.previous;
+		next.previous = previous;
+		previous.next = next;
 	}
+	public String find(String color){
+		Link temp = head;
+		while(temp.colorName != color){
+			temp = temp.next;
+		}
+		
+		return temp.colorName;
 	}
-	
-	
-	
-	
-	
 }
+	
+	
+	
+	
+	
+
--- Test.java
+++ Test.java
@@ -1,16 +1,49 @@
 
+
 public class Test {
 
+	
 
 public static void main(String[] args) //Main Method
 {
+
+	
 	DoublyLinkedList test = new DoublyLinkedList();
+	
+	test.addStart("Yellow");
 	test.addStart("Blue");
 	test.addStart("Green");
 	test.addStart("Purple");
+	
+	
+	
 	test.printListForward();
 	System.out.println();
 	test.printListBackward();
+	System.out.println();
+	
+	System.out.println("Alphabetically: ");
+	DoublyLinkedList test2 = new DoublyLinkedList();
+	test2.addAlphabetical("Orange");
+	test2.addAlphabetical("Red");
+	test2.addAlphabetical("Blue");
+	test2.addAlphabetical("Maroon");
+	
+	test2.printListForward();
+	System.out.println();
+	test2.printListBackward();
+	System.out.println();
+	
+	System.out.println("Deleting the Link containing Maroon: ");
+	test2.deleteLink("Maroon");
+	test2.printListForward();
+	System.out.println();
+	test2.printListBackward();
+	
+	System.out.println("\nNow finding the Link that contains Blue: ");
+	
+	System.out.println(test2.find("Blue"));
+	
 	
 }
 }
